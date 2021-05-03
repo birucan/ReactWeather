70f69abb083478304515aa42e9d5aeef320f8b21
Tomás Kavanagh <birucandev@gmail.com>
2021-05-03 02:26:03 -0500

    Revert "finished"
    This reverts commit 36ece15e5d19134293f047a895a2f854123a7310.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from "react";
-import "./App.css";
+import logo from './logo.svg';
+import React from 'react'
+import './App.css';
 
 //Local component imports
-import { AppBarUI } from "./components/AppBar.js";
-import { Content } from "./components/Content.js";
+import {AppBarUI} from './components/AppBar.js'
+import {Content} from './components/Content.js'
+
+
 
 function App() {
   //This state hook handles what page is loaded
-  const [pageState, setPageState] = React.useState("weather");
+  const [pageState, setPageState] = React.useState('weather')
 
   //this is the handler for the page state
-  function pageHandler(value) {
-    setPageState(value);
+  function pageHandler(value){
+    setPageState(value)
   }
-
+  
   return (
     <div className="App">
-      <div className="appBar">
-        <AppBarUI pageState={pageState} setPageState={pageHandler} />
-      </div>
-      <Content pageState={pageState} />
+    <div className="appBar">
+      <AppBarUI pageState={pageState} setPageState={pageHandler}/>
     </div>
+      <Content pageState={pageState}/>
+    </div>
+    
   );
 }
 
